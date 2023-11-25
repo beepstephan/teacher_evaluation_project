7082a71e5024a54daeeb4818c4fe99ae70f36dd1
Ihor Stepanov <igorka2005@gmail.com>
2023-11-25 19:24:23 +0200

    User and Teacher Classes Update

--- projectClasses/Teacher.cs
+++ projectClasses/Teacher.cs
@@ -8,5 +8,14 @@ namespace teacher_evaluation_project.projectClasses
 {
     internal class Teacher
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MiddleName { get; set; }
+        public string Position { get; set; }
+        public string Description { get; set; }
+        public string Subjects { get; set; }
+        public double Rate { get; set; }
+        // методи для отримання інфи про них
     }
 }
