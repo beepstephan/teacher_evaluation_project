55fbfd521d28fb2dc9ce13d7eae62ecff09e626f
Donte <stas.zp.619@gmail.com>
2023-11-28 16:26:50 +0200

    +default quicksort

--- projectClasses/Sort.cs
+++ projectClasses/Sort.cs
@@ -8,5 +8,35 @@ namespace teacher_evaluation_project.projectClasses
 {
     internal class Sort
     {
+        static List<string> QuickSort(List<string> list)
+        {
+            if (list.Count <= 1)
+                return list;
+
+            int pivotIndex = list.Count / 2;
+            string pivot = list[pivotIndex];
+            List<string> left = new List<string>();
+            List<string> right = new List<string>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == pivotIndex)
+                    continue;
+
+                if (String.Compare(list[i], pivot) <= 0)
+                {
+                    left.Add(list[i]);
+                }
+                else
+                {
+                    right.Add(list[i]);
+                }
+            }
+
+            List<string> sorted = QuickSort(left);
+            sorted.Add(pivot);
+            sorted.AddRange(QuickSort(right));
+            return sorted;
+        }
     }
 }
