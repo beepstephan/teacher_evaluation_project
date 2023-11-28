54f39ac0fee1549e027a06fb887815ccd387bcc5
SmokinSexyStas <stas.zp.619@gmail.com>
2023-11-28 20:54:12 +0200

    Sort v0.9

--- projectClasses/Sort.cs
+++ projectClasses/Sort.cs
@@ -8,22 +8,23 @@ namespace teacher_evaluation_project.projectClasses
 {
     internal class Sort
     {
-        static List<string> QuickSort(List<string> list)
+
+        static List<Teacher> SurnameAscending(List<Teacher> list)
         {
             if (list.Count <= 1)
                 return list;
 
             int pivotIndex = list.Count / 2;
-            string pivot = list[pivotIndex];
-            List<string> left = new List<string>();
-            List<string> right = new List<string>();
+            Teacher pivot = list[pivotIndex];
+            List<Teacher> left = new List<Teacher>();
+            List<Teacher> right = new List<Teacher>();
 
             for (int i = 0; i < list.Count; i++)
             {
                 if (i == pivotIndex)
                     continue;
 
-                if (String.Compare(list[i], pivot) <= 0)
+                if (String.Compare(list[i].Surname, pivot.Surname) <= 0)
                 {
                     left.Add(list[i]);
                 }
@@ -33,9 +34,102 @@ namespace teacher_evaluation_project.projectClasses
                 }
             }
 
-            List<string> sorted = QuickSort(left);
+            List<Teacher> sorted = SurnameAscending(left);
             sorted.Add(pivot);
-            sorted.AddRange(QuickSort(right));
+            sorted.AddRange(SurnameAscending(right));
+            return sorted;
+        }
+
+        static List<Teacher> SurnameDescending(List<Teacher> list)
+        {
+            if (list.Count <= 1)
+                return list;
+
+            int pivotIndex = list.Count / 2;
+            Teacher pivot = list[pivotIndex];
+            List<Teacher> left = new List<Teacher>();
+            List<Teacher> right = new List<Teacher>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == pivotIndex)
+                    continue;
+
+                if (String.Compare(list[i].Surname, pivot.Surname) > 0)
+                {
+                    left.Add(list[i]);
+                }
+                else
+                {
+                    right.Add(list[i]);
+                }
+            }
+
+            List<Teacher> sorted = SurnameDescending(left);
+            sorted.Add(pivot);
+            sorted.AddRange(SurnameDescending(right));
+            return sorted;
+        }
+
+        static List<Teacher> RateAscending(List<Teacher> list)
+        {
+            if (list.Count <= 1)
+                return list;
+
+            int pivotIndex = list.Count / 2;
+            Teacher pivot = list[pivotIndex];
+            List<Teacher> left = new List<Teacher>();
+            List<Teacher> right = new List<Teacher>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == pivotIndex)
+                    continue;
+
+                if (list[i].Rate <= pivot.Rate)
+                {
+                    left.Add(list[i]);
+                }
+                else
+                {
+                    right.Add(list[i]);
+                }
+            }
+
+            List<Teacher> sorted = RateAscending(left);
+            sorted.Add(pivot);
+            sorted.AddRange(RateAscending(right));
+            return sorted;
+        }
+
+        static List<Teacher> RateDescending(List<Teacher> list)
+        {
+            if (list.Count <= 1)
+                return list;
+
+            int pivotIndex = list.Count / 2;
+            Teacher pivot = list[pivotIndex];
+            List<Teacher> left = new List<Teacher>();
+            List<Teacher> right = new List<Teacher>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == pivotIndex)
+                    continue;
+
+                if (list[i].Rate >= pivot.Rate)
+                {
+                    left.Add(list[i]);
+                }
+                else
+                {
+                    right.Add(list[i]);
+                }
+            }
+
+            List<Teacher> sorted = RateDescending(left);
+            sorted.Add(pivot);
+            sorted.AddRange(RateDescending(right));
             return sorted;
         }
     }
