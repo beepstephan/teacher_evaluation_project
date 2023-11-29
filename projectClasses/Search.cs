e43b63539bc9069269c880add55aa4a498b2cd02
SmokinSexyStas <stas.zp.619@gmail.com>
2023-11-29 14:00:14 +0200

    class Search added

---
+++ projectClasses/Search.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teacher_evaluation_project.projectClasses
+{
+    internal class Search
+    {
+        static List<Teacher> SearchByName(List<Teacher> list, string input)
+        {
+            List<Teacher> result = new List<Teacher>();
+
+
+
+            return result;
+        }
+    }
+}
